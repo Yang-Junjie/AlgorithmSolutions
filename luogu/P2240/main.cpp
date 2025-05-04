c7045bc834b805f271e6f2b72134a9ee4e925d67
oxygen <1973690778@qq.com>
2025-05-04 19:26:51 +0800

    update

--- luogu/P2240/main.cpp
+++ luogu/P2240/main.cpp
@@ -1,69 +1,81 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <utility>
-#include <vector>
-#include <iomanip>
-#include <numeric>
+
+#include <bits/stdc++.h>
 using namespace std;
 
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
 
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int, int>;
+
+#define DEBUG
 #ifdef DEBUG
-#define LOG1(a) cerr << "[" << #a << ":" << (a) << "]" << endl
-#define LOG2(a,b) cerr << "[" << #a << ":" << (a) <<","<< #b << ":" << (b) << "]" << endl
-template<typename Container>
-void PrintCon(const Container& cont) {
+template <typename Container>
+void PrintCon(const Container& cont)
+{
     std::cout << "{ ";
     if (!cont.empty()) {
         auto it = cont.begin();
-        std::cout <<"\033[33m"<< *it++<< "\033[0m";
+        std::cout << "\033[33m" << *it++ << "\033[0m";
         for (; it != cont.end(); ++it)
-            std::cout << " | " << *it;
+            std::cout << " | " << "\033[33m" << *it << "\033[0m";
     }
     std::cout << " }" << std::endl;
 }
 
+template <typename... Args>
+void LOG(Args&&... values)
+{
+    ((std::cout << "\033[33m" << std::forward<Args>(values) << "\033[0m" << " "), ...);
+    std::cout << std::endl;
+}
+
 #else
-#define LOG(a)
+
+template <typename... Args>
+void LOG(Args&... args);
+template <typename Container>
+void PrintCon(const Container& cont);
+
 #endif
+struct Data {
+    double v;
+    double w;
+    double m;
+};
 
-#define LEN(a) ((int)(a).size())
-#define ALL(x) (x).begin(), (x).end()
-using ll = long long int;
-using ull = unsigned long long int;
-using PII = pair<int,int>;
-using VI = vector<int>;
-
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n,t;
-    cin>>n>>t;
-    vector<double> a(n);
-    vector<double> w(n);
-    vector<double> v(n);
-    for(int i = 0;i<n;i++){ 
-        cin>>w[i]>>v[i];
-        a[i] = v[i]/w[i];
+    double n, v;
+    cin >> n >> v;
+    vector<Data> a(n);
+    for (int i = 0; i < n; i++) {
+        double s, c;
+        cin >> s >> c;
+        Data tmp;
+        tmp.w = s;
+        tmp.m = c;
+        tmp.v = c / s;
+        a[i]=tmp;
     }
-    sort(ALL(a));
-    double nowv = 0;
+    sort(ALL(a), [](Data a, Data b) {
+        return a.v > b.v;
+    });
     double ans = 0;
-    for(int i = 0;i<n;i++){
-        if(t-w[i]>-0.000001)
-		{
-			t-=w[i];
-			ans+=v[i];
-		}
-		else
-		{
-			ans+=t*a[i];
-			break;
-		}
+    for(auto i :a){
+        if(v>=i.w){
+            v-=i.w;
+            ans+=i.m;
+        }else{
+            double value = v*i.v;
+            ans+=value;
+            v-=v;
+        }
     }
     cout<<setprecision(2)<<fixed<<ans<<endl;
-
-    
-
     return 0;
 }
