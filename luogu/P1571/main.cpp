15a60fb3535f2b479acba9fa0e840bfe773018de
Yang-Junjie <1973690778@qq.com>
2025-05-11 20:23:07 +0800

    update

---
+++ luogu/P1571/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
+
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int, int>;
+
+#define DEBUG
+#ifdef DEBUG
+template <typename Container>
+void PrintCon(const Container &cont)
+{
+    std::cout << "{ ";
+    if (!cont.empty())
+    {
+        auto it = cont.begin();
+        std::cout << "\033[33m" << *it++ << "\033[0m";
+        for (; it != cont.end(); ++it)
+            std::cout << " | " << "\033[33m" << *it << "\033[0m";
+    }
+    std::cout << " }" << std::endl;
+}
+
+template <typename... Args>
+void LOG(Args &&...values)
+{
+    ((std::cout << "\033[33m" << std::forward<Args>(values) << "\033[0m" << " "), ...);
+    std::cout << std::endl;
+}
+
+#else
+
+template <typename... Args>
+void LOG(Args &...args);
+template <typename Container>
+void PrintCon(const Container &cont);
+
+#endif
+
+bool BS(ll x, const vector<ll>& a)
+{
+    int l = -1, r = a.size();
+    while (l + 1 != r)
+    {
+        int mid = l + r >> 1;
+        if (a[mid] == x)
+        {
+            return true;
+        }
+        if (a[mid] >= x)
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid;
+        }
+    }
+
+    if (a[r] == x)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+    }
+    sort(ALL(b));
+    for (auto i : a)
+    {
+        if (BS(i, b))
+        {
+            cout << i << ' ';
+        }
+    }
+    return 0;
+}
