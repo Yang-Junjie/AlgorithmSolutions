ab256071d77bcfb6bc5ae9c003e5e0dcadc33931
Yang-Junjie <1973690778@qq.com>
2025-05-23 23:44:28 +0800

    update

---
+++ codeforces/1154A/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
+
+
+
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int,int>;
+
+#define DEBUG
+#ifdef DEBUG
+template<typename Container>
+void PrintCon(const Container& cont) {
+    std::cout << "{ ";
+    if (!cont.empty()) {
+        auto it = cont.begin();
+        std::cout <<"\033[33m"<< *it++<< "\033[0m";
+        for (; it != cont.end(); ++it)
+            std::cout << " | " <<"\033[33m"<< *it<< "\033[0m";
+    }
+    std::cout << " }" << std::endl;
+}
+
+template<typename... Args>
+void LOG(Args&&... values) {
+    ((std::cout <<"\033[33m"<< std::forward<Args>(values) << "\033[0m"<< " "), ...);
+    std::cout << std::endl;
+}
+
+#else
+
+template<typename ...Args>
+void LOG(Args& ...args);
+template<typename Container>
+void PrintCon(const Container& cont);
+
+#endif
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<ll> a(4);
+    for(int i = 0;i<4;i++){
+      cin>>a[i];
+    }
+    sort(ALL(a));
+    for(int i =2;i>=0;i--){
+      cout<<a[3]-a[i]<<' ';
+    }
+    cout<<endl;
+    
+
+    return 0;
+}
