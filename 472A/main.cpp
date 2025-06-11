e5237cc448a29ab84b88fd5b0a330fe29e2e1af6
Yang-Junjie <1973690778@qq.com>
2025-06-11 23:43:43 +0800

    update

---
+++ 472A/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
+
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int, int>;
+
+#define DEBUG
+#ifdef DEBUG
+template <typename Container>
+void PrintCon(const Container &cont)
+{
+    std::cout << "{ ";
+    if (!cont.empty())
+    {
+        auto it = cont.begin();
+        std::cout << "\033[33m" << *it++ << "\033[0m";
+        for (; it != cont.end(); ++it)
+            std::cout << " | " << "\033[33m" << *it << "\033[0m";
+    }
+    std::cout << " }" << std::endl;
+}
+
+template <typename... Args>
+void LOG(Args &&...values)
+{
+    ((std::cout << "\033[33m" << std::forward<Args>(values) << "\033[0m" << " "), ...);
+    std::cout << std::endl;
+}
+
+#else
+
+template <typename... Args>
+void LOG(Args &...args);
+template <typename Container>
+void PrintCon(const Container &cont);
+
+#endif
+bool isPrime(int x)
+{
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0)
+            return true;
+    return false;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        if (isPrime(i) && isPrime(n - i))
+        {                              
+            cout << i << " " << n - i; 
+            return 0;                 
+        }
+    return 0;
+}
