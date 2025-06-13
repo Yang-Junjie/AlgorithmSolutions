dba36e9d3b383d75361b3460a305dad2078b31c9
Yang-Junjie <1973690778@qq.com>
2025-06-13 22:50:49 +0800

    update

---
+++ codeforces/1087B/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
+
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int, int>;
+
+#define DEBUG
+
+#ifdef DEBUG
+template <typename Container>
+void PrintCon(const Container &cont)
+{
+    std::cout << "{ ";
+    if (!cont.empty())
+    {
+        auto it = cont.begin();
+        std::cout << "\033[33m" << *it++ << "\033[0m";
+        for (; it != cont.end(); ++it)
+            std::cout << " | " << "\033[33m" << *it << "\033[0m";
+    }
+    std::cout << " }" << std::endl;
+}
+
+template <typename... Args>
+void LOG(Args &&...values)
+{
+    ((std::cout << "\033[33m" << std::forward<Args>(values) << "\033[0m" << " "), ...);
+    std::cout << std::endl;
+}
+#else
+template <typename... Args>
+void LOG(Args &...args);
+
+template <typename Container>
+void PrintCon(const Container &cont);
+#endif
+
+void solve()
+{
+    int n, cnt1 = 0, cnt2 = 0;
+    cin >> n;
+    vector<int> a(101);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (a[i] & 1)
+        {
+            cnt1 += a[i];
+        }
+        else
+        {
+            cnt2 += a[i];
+        }
+    }
+    cout << (cnt2 > cnt1 ? "YES" : "NO") << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ codeforces/149A/main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(x) (x).begin(), (x).end()
+#define MAXV(a, l, r) *std::max_element((a).begin() + (l), (a).begin() + (r))
+#define MINV(a, l, r) *std::min_element((a).begin() + (l), (a).begin() + (r))
+
+using ll = long long int;
+using ull = unsigned long long int;
+using PII = pair<int, int>;
+
+#define DEBUG
+#ifdef DEBUG
+template <typename Container>
+void PrintCon(const Container &cont)
+{
+    std::cout << "{ ";
+    if (!cont.empty())
+    {
+        auto it = cont.begin();
+        std::cout << "\033[33m" << *it++ << "\033[0m";
+        for (; it != cont.end(); ++it)
+            std::cout << " | " << "\033[33m" << *it << "\033[0m";
+    }
+    std::cout << " }" << std::endl;
+}
+
+template <typename... Args>
+void LOG(Args &&...values)
+{
+    ((std::cout << "\033[33m" << std::forward<Args>(values) << "\033[0m" << " "), ...);
+    std::cout << std::endl;
+}
+
+#else
+
+template <typename... Args>
+void LOG(Args &...args);
+template <typename Container>
+void PrintCon(const Container &cont);
+
+#endif
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n = 0;
+    cin >> n;
+    array<int, 12> a;
+    for (int i = 0; i < 12; i++)
+        cin >> a[i];
+
+    sort(ALL(a), [](int a, int b)
+         { return a > b; });
+
+    int sum = 0;
+    for (int i = 0; i <= 12; i++)
+    {
+        if (sum >= n)
+
+        {
+            cout << i << endl;
+            return 0;
+        }
+        if (i < 12)
+            sum += a[i];
+    }
+    cout << -1 << endl;
+    return 0;
+}
